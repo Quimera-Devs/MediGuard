002787cf3416cfbd28eb1bc39ed3042b283323de
Lenovo <javier.gianneo@gmail.com>
2021-04-02 22:37:04 -0300

    nombres en ingles y version minsdk

--- app/src/main/java/com/programabit/mediguard/AvaibleGuards.java
+++ app/src/main/java/com/programabit/mediguard/AvaibleGuards.java
@@ -3,11 +3,11 @@ package com.programabit.mediguard;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class GuardiasDisponibles extends AppCompatActivity {
+public class AvaibleGuards extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
-        setContentView(R.layout.guardias_disponibles);
+        setContentView(R.layout.avaible_guards);
     }
 }
