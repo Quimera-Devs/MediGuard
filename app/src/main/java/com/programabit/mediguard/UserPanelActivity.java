526eed62d37b2380c253efa1f78a572ec75660ca
Matias Mutter <matimutter@gmail.com>
2021-04-06 03:32:51 -0300

    toolbar working in progress

--- app/src/main/java/com/programabit/mediguard/UserPanelActivity.java
+++ app/src/main/java/com/programabit/mediguard/UserPanelActivity.java
@@ -3,36 +3,30 @@ package com.programabit.mediguard;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity {
+public class UserPanelActivity extends AppCompatActivity {
 
-    TextView username;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
-
-        username = findViewById(R.id.username);
-
-        Intent intent = getIntent();
-
-        if(intent.getExtras() != null){
-            String passedUsername = intent.getStringExtra("data");
-            username.setText("Welcome "+passedUsername);
-        }
+        setContentView(R.layout.activity_user_panel);
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        appToolbar(toolbar, R.string.activity_name_dashboard,false);
+        appToolbar(toolbar, R.string.activity_name_user_panel,true);
     }
 
     private void appToolbar(Toolbar toolbar, int activity_name, boolean enable) {
         setSupportActionBar(toolbar);
         getSupportActionBar().setSubtitle(activity_name);
         getSupportActionBar().setDisplayHomeAsUpEnabled(enable);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(UserPanelActivity.this,"prueba", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
