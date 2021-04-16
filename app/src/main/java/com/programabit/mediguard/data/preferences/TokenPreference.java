7d8ac49a4a9faa2aada9defb1b60d30a6919eb9e
Lenovo <javier.gianneo@gmail.com>
2021-04-16 00:32:21 -0300

    Alcance global al token

---
+++ app/src/main/java/com/programabit/mediguard/data/preferences/TokenPreference.java
@@ -0,0 +1,23 @@
+package com.programabit.mediguard.data.preferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class TokenPreference {
+
+    private final String KEY_TOKEN = TokenPreference.class.getSimpleName() + "token";
+
+    private final SharedPreferences preferences;
+
+    public TokenPreference(Context context) {
+        this.preferences = context.getSharedPreferences(KEY_TOKEN, Context.MODE_PRIVATE);
+    }
+
+    public void saveToken(String token) {
+        this.preferences.edit().putString(KEY_TOKEN, token).apply();
+    }
+
+    public String getToken() {
+        return this.preferences.getString(KEY_TOKEN, "");
+    }
+}
